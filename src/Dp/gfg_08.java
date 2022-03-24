50563566823a74e8ea8145855013617f8aa984d0
Shreyansh <aayansrivastava1912@gmail.com>
2022-03-24 22:17:54 +0530

    Fibonacci Number

---
+++ src/Dp/gfg_08.java
@@ -0,0 +1,21 @@
+package Dp;
+
+import java.util.HashMap;
+
+class gfg_08 {
+    public int fib(int n) {
+        return nthFib(n,new HashMap<Integer,Integer>());
+    }
+    public int nthFib(int n,HashMap<Integer,Integer> memo)
+    {
+        if(n<=1)
+            return n;
+
+        if(memo.containsKey(n))
+            return memo.get(n);
+
+        memo.put(n,nthFib(n-1,memo) + nthFib(n-2,memo));
+
+        return memo.get(n);
+    }
+}
