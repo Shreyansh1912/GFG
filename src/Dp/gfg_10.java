e9921b59bb3ca48915a3620fa68518c4342d176d
Shreyansh <aayansrivastava1912@gmail.com>
2022-03-25 23:42:53 +0530

    Unique Binary Search Tree

---
+++ src/Dp/gfg_10.java
@@ -0,0 +1,27 @@
+package Dp;
+
+import java.util.HashMap;
+
+class gfg_10 {
+    public int numTrees(int n) {
+        return nthCatalan(n,new HashMap<Integer, Integer>());
+    }
+    public Integer nthCatalan(int n, HashMap<Integer, Integer> memo){
+        if(n <= 1){
+            return 1;
+        }
+
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int num = 0;
+
+        for(int i = 0; i < n; i++){
+            num += nthCatalan(i, memo) * nthCatalan(n -i -1, memo);
+        }
+
+        memo.put(n, num);
+        return memo.get(n);
+    }
+}
